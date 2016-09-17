a1c505b86b3a8e75f0094b1b765bf58f830bf580
sardkit <963831134@qq.com>
2016-09-17 20:33:07 +0800

    changed in 09-17 20:33

--- redis_test/main.c
+++ redis_test/main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     conn = rop_connectdb_nopwd("127.0.0.1", "6379");
 
     //set foo  nihao
-    ret = rop_set_string(conn, "foo", "nihao");
+    ret = rop_set_string(conn, "foo", "你好");
     if (ret == 0) {
         printf("set succ!\n");
     }
